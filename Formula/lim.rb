7d32e7df24297c7d51c9903e0b4519e877c64ccf
karlimbar <karlimbar@users.noreply.github.com>
2024-08-17 20:52:32 +0000

    Update version to v0.7.0

--- Formula/lim.rb
+++ Formula/lim.rb
@@ -1,26 +1,26 @@
 class Limbar < Formula
   desc "Get remote Android instances for local development and testing"
   homepage "https://limbar.io"
-  version "v0.1.0"
+  version "v0.7.0"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/limbario/homebrew-lim/releases/download/v#{version}/lim-#{version}-darwin-arm64.tar.gz"
-      sha256 "replace_with_darwin_arm64_sha256" # replace_with_darwin_arm64_sha256
+      sha256 "" # replace_with_darwin_arm64_sha256
     else
       url "https://github.com/limbario/homebrew-lim/releases/download/v#{version}/lim-#{version}-darwin-amd64.tar.gz"
-      sha256 "replace_with_darwin_amd64_sha256" # replace_with_darwin_amd64_sha256
+      sha256 "" # replace_with_darwin_amd64_sha256
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/limbario/homebrew-lim/releases/download/v#{version}/lim-#{version}-linux-arm64.tar.gz"
-      sha256 "replace_with_linux_arm64_sha256" # replace_with_linux_arm64_sha256
+      sha256 "" # replace_with_linux_arm64_sha256
     else
       url "https://github.com/limbario/homebrew-lim/releases/download/v#{version}/lim-#{version}-linux-amd64.tar.gz"
-      sha256 "replace_with_linux_amd64_sha256" # replace_with_linux_amd64_sha256
+      sha256 "" # replace_with_linux_amd64_sha256
     end
   end
 
